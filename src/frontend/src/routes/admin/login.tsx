592613f5440bb0dae303767a1787240f8232fed4
Kin LI <casper.hk@outlook.com>
2026-07-22 23:01:43 +0800

    feat: update admin login route to use AdminLoginPage component

--- src/frontend/src/routes/admin/login.tsx
+++ src/frontend/src/routes/admin/login.tsx
@@ -1,9 +1,10 @@
-import { createFileRoute } from '@tanstack/solid-router'
+import { createFileRoute } from "@tanstack/solid-router";
+import AdminLoginPage from "../../pages/AdminLoginPage";
 
-export const Route = createFileRoute('/admin/login')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/admin/login")({
+  component: AdminLoginRoute,
+});
 
-function RouteComponent() {
-  return <div>Hello "/admin/login"!</div>
+function AdminLoginRoute() {
+  return <AdminLoginPage />;
 }
